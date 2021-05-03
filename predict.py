d4dbfd85660333fa9ecb6be694aef44e813ad997
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2021-05-03 14:31:42 +0530

    first commit

---
+++ predict.py
@@ -0,0 +1,5 @@
+from preprocessing import Preprocessing
+#using preprocessed data predict y and calculate accuracy 
+X_train,X_test,y_train,y_test = Preprocessing()
+
+print(len(X_train),len(X_test),len(y_train),len(y_test))
